0520a6e02b201d04489a6eaa402acf9d9003f730
umbertodefilippis <umberto.defilippis@uniroma1.it>
2025-12-22 17:00:13 +0100

    Final version of HR cal update

--- src/common_routines/raw2eng.py
+++ src/common_routines/raw2eng.py
@@ -28,14 +28,12 @@ def raw2eng(par_data, calibration, product_type, dyn):
             par['data'] = [value  for value in par['data']]
         if product_type == 'sc_hr' and name_text in ['POS']:
             axes = [str(a) for a in par_data['ACC_ACQUIRED']['data']]
-            print("DEBUG calibration keys:", [calibration[f'Acc{axis}']['alpha']['data'] for axis in axes])            
             par['data'] = [
                 value
                 * calibration[f'Acc{axis}']['alpha']['data']
                 * calibration['HR']['alpha']['data']
                 for value, axis in zip(par['data'], axes)
             ]
-            print(par['data'])
         if product_type == 'sc_hr' and name_text == 'INTERPOLATED':
             par['data'] = [value  for value in par['data']]
 #### TBC ####
--- src/raw2cal.py
+++ src/raw2cal.py
@@ -398,10 +398,8 @@ for filename in os.listdir(pds_folder):
                         'data': par_data[key], 
                         }
 # Compute HAA selected dynamic  
-            print(par_data2.keys())
             dyn = generate_dyn_vector(par_data, service_events)
             eng = raw2eng.raw2eng(par_data2, calibration, product_type, dyn)
-            print(eng.keys())
             write_log(
                 log_path=log_file_path,
                 severity="INFO",
